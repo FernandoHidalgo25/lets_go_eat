ed79abca976273467a5a0cfbe9d6b2b92484e4ae
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-28 07:57:55 +0000

    edit restaurants

--- lib/restaurants.rb
+++ lib/restaurants.rb
@@ -1,13 +1,9 @@
 class Restaurants
-
   attr_accessor :name, :formatted_address, :opening_hours, :rating, :user_ratings_total
   # through a reader and a writer by using attr_accessor to make the variables available outside of the class
-
   @@all = []  # the class variable array
-
   #------------------------------------------------------------------------------------
   def initialize(name, formatted_address, opening_hours, rating, user_ratings_total)  # this method creates a new instances of a
-binding.pry
     @name = name
     @formatted_address = formatted_address
     @opening_hours = opening_hours
